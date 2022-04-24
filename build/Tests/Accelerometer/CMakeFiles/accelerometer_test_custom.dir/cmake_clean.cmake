6972e25b8d6ad31162779ce09603ad2d153c998a
Vladimir Mishel <43666597+vmishelcs@users.noreply.github.com>
2022-04-24 02:11:39 -0700

    Delete build directory

--- build/Tests/Accelerometer/CMakeFiles/accelerometer_test_custom.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/accelerometer_test_custom"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/accelerometer_test_custom.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
